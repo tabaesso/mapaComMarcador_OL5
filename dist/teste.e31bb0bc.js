54e8fe694c4b358a57df1952ffe746f106977743
tabaesso <tatabaesso@gmail.com>
2019-01-14 09:19:20 -0200

    files

--- dist/teste.e31bb0bc.js
+++ dist/teste.e31bb0bc.js
@@ -47719,7 +47719,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46402" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40260" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
